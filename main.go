ec4a258bd80e9760d3064a280ac22fd8bf229f3c
Avin Ben-Shemesh <ambrown111592@yahoo.com>
2022-10-27 10:44:59 -0400

    added more logic to the sendmoney function.

--- main.go
+++ main.go
@@ -18,15 +18,25 @@ func main(){
 	account2.HolderName = "Bob Man"
 	account2.AcccountNumber = 94495694849549
 	account2.AccountBalance = 949598
+
+	account3 := new(BankAccountInfo);
+	account3.HolderName = "Tom Hardey"
+	account3.AcccountNumber = 04050450;
+	account3.AccountBalance = 0
+	account3.checkBalance()
 	
 	sendMoney(account1, account2)
-	// account1.checkBalance()
-	// account2.checkBalance()
-	// account1.validAccount()
+	account1.checkBalance()
+	account1.validAccount()
+
+	account2.checkBalance()
 }
-func (b *BankAccountInfo) checkBalance() {
+func (b *BankAccountInfo) checkBalance() bool {
 	if b.AccountBalance > 0 {
 		fmt.Printf("User %s has an account balance of $%f\n", b.HolderName, b.AccountBalance)
+		return true
+	} else {
+		return false
 	}
 }
 func (b *BankAccountInfo) validAccount() bool {
@@ -36,13 +46,15 @@ func (b *BankAccountInfo) validAccount() bool {
 	return false
 }
 func sendMoney(b1 *BankAccountInfo, b2 *BankAccountInfo) {
+	if b1.checkBalance() == true {
+	}
 	var answer string;
 	fmt.Println("Welcome")
 	fmt.Println("would you like to send money")
 	fmt.Scan(&answer)
 	if answer == "yes" || answer == "YES" || answer == "y" || answer == "Y" {
 		fmt.Println("Your answer was yes")
-	}
+	} else { fmt.Println("Please come back again.")}
 	if b1.AcccountNumber < 0 {
 		if b1.AccountBalance > 0 {
 		}
