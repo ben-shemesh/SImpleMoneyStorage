f5f20980dc482051a01b34d433b91586d52f414e
Avin Ben-Shemesh <ambrown111592@yahoo.com>
2022-10-27 09:44:27 -0400

    moved functionality around.

--- main.go
+++ main.go
@@ -18,10 +18,13 @@ func main(){
 	account2.HolderName = "Bob Man"
 	account2.AcccountNumber = 94495694849549
 	account2.AccountBalance = 949598
-	checkBalance(account1)
+	
+	account1.checkBalance()
+	account2.checkBalance()
+	account1.validAccount()
 	sendMoney(account1, account2)
 }
-func checkBalance(b *BankAccountInfo) {
+func (b *BankAccountInfo) checkBalance() {
 	if b.AccountBalance > 0 {
 		fmt.Printf("User %s has an account balance of $%f\n", b.HolderName, b.AccountBalance)
 	}
@@ -29,7 +32,7 @@ func checkBalance(b *BankAccountInfo) {
 func (b *BankAccountInfo) validAccount() bool {
 	if b.AcccountNumber == 0 {
 		fmt.Println("There is no account.")
-	}
+	}else {fmt.Println("There is an account associated with this account number.")}
 	return false
 }
 func sendMoney(b1 *BankAccountInfo, b2 *BankAccountInfo) {
